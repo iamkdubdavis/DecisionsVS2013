76a68a5ab42631fe2ec5523158a26057efd16d6b
Kimberly Davis <kwatsondavis@gmail.com>
2015-08-17 20:39:28 -0400

    First C# upload!

---
+++ Decisions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please type something and press the Enter key.");
+            //string userValue;
+            //userValue = Console.ReadLine();
+            //Console.WriteLine("You typed: " + userValue);
+
+            Console.WriteLine("Would you prefer what is behind door number 1, 2, or 3?");
+            string userValue = Console.ReadLine();
+
+            //string message = " ";
+            //if (userValue == "1") {
+            //    message = ("You won a new car!");
+            //}
+            //else if (userValue == "2") {
+            //    message = ("You won a new boat!");
+            //}
+            //else if (userValue == "3") {
+            //    message = ("You won a pool table!");
+            //}
+            //else {
+            //    message = ("You got ZONKED! Sucka!!!");
+            //}
+            //Console.WriteLine(message);
+
+            string message = (userValue == "1") ? "boat" : "shred of lint";
+            Console.WriteLine("You won a {0}", message);
+            Console.ReadLine();
+        }
+    }
+}
